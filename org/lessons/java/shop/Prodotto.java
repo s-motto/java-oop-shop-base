ed9d49e5b230c1a2b814c1c790bf6424689a9a7e
s-motto <brinamotto@gmail.com>
2024-06-19 21:57:28 +0200

    added prezzoPiuIva()

--- org/lessons/java/shop/Prodotto.java
+++ org/lessons/java/shop/Prodotto.java
@@ -16,13 +16,6 @@ public class Prodotto {
 	//trasformo double in int per eliminare il .0 finale
 	int codiceFinale=(int) codiceRandom();
 	
-	//calcolo prezzo finale
-	float prezzoPiuIva() {
-		float percentuale=prezzo*iva;
-		return prezzo+percentuale;
-	}
-	
-	float prezzoFinale=prezzoPiuIva();
 	
 	Prodotto(double codice, String nome, String descrizione, float prezzo, int iva){
 		this.codice= codiceRandom();
@@ -33,9 +26,19 @@ public class Prodotto {
 				
 	};
 	
+	//calcolo prezzo finale
+			float prezzoPiuIva() {
+		 
+				float percentuale=prezzo+((prezzo*iva)/100);
+				return percentuale;
+			}
+			
+			
+
+	
 	//stringa con le info del prodotto
 	String infoProdotto(){
-		return "Codice "+codiceFinale+", "+nome+", "+descrizione+", "+ prezzo+"euro, iva al "+iva+"%"+". Prezzo finale: "+prezzoFinale;
+		return "Codice "+codiceFinale+", "+nome+", "+descrizione+", "+ prezzo+"euro, iva al "+iva+"%"+". Prezzo finale: "+prezzoPiuIva();
 	};
 		
 		
@@ -46,8 +49,8 @@ public class Prodotto {
         
 		//lista prodotti
 		Prodotto pasta= new Prodotto(0000, "Mezze penne Barilla", "Mezze penne rigate, da grano italiano", 2.45f, 20);
-		Prodotto uova= new Prodotto(0000, "Uova Esselunga, 6pz", "uova da galline allevate a terra", 3.22f, 10);
-		Prodotto salsaChilli= new Prodotto(0000, "Sweet Chilli Sauce Go-Tan", "salsa di peperoncino chili dolce", 4.76f,22);		
+		Prodotto uova= new Prodotto(0000, "Uova Esselunga, 6pz", "uova da galline allevate a terra", 3.99f, 10);
+		Prodotto salsaChilli= new Prodotto(0000, "Sweet Chilli Sauce Go-Tan", "salsa di peperoncino chili dolce", 4.34f,22);		
 		
 		
 		System.out.println(pasta.infoProdotto());
